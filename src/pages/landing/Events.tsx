478a397af6dac210a7ad60c452290576669cac2d
David Glymph <Woozl@users.noreply.github.com>
2025-11-10 16:15:10 -0500

    update office hours time

--- src/pages/landing/Events.tsx
+++ src/pages/landing/Events.tsx
@@ -4,7 +4,7 @@ function Events() {
   return (
     <Section title="Events" index={5}>
       <p>
-        ROBOKOP has weekly office hours from 2-3pm EST.{' '}
+        ROBOKOP has weekly office hours from 10-11am EST.{' '}
         <a
           href="https://outlook.office365.com/owa/calendar/f78771fc86a544fa87ba0c0d3e2eb20e@renci.org/f584171e733046f88746dc0c2d605b3815090452925078675798/calendar.html"
           target="_blank"
