0cc3bcc61aa0319222e1a040215b4c3f89ca24ad
Amrutesh Arun <amrutesharun0599@gmail.com>
2026-07-20 10:10:02 -0400

    Grammatical error correction for testing

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -30,7 +30,9 @@ export default function LandingPage() {
           </p>
         </Card>
         <Card title='Graph Registry' href='/graphs' icon={<DataIcon />} gradient='purple'>
-          <p>Learn about the data in ROBOKOP and explore knowledge graph using our data browser.</p>
+          <p>
+            Learn about the data in ROBOKOP and explore the knowledge graph using our data browser.
+          </p>
         </Card>
       </CardContainer>
     </Section>
